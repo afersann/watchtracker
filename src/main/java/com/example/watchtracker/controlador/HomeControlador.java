fad2e0ac5cb80ed191d650a7beba046b78e1c4c1
afersann <afersan07@gmail.com>
2025-06-17 09:41:44 +0200

    Modificacion prueba homecontrolador

--- src/main/java/com/example/watchtracker/controlador/HomeControlador.java
+++ src/main/java/com/example/watchtracker/controlador/HomeControlador.java
@@ -1,3 +1,9 @@
+package com.example.watchtracker.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HomeControlador {
 
